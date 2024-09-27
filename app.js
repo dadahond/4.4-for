d89a036484d29a6a117579052e97fe8b04730904
dadahon <dadahon@gmail.com>
2024-09-27 20:24:35 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// FOR 1
+// let k = prompt("k sonini kiriting");
+// let n = prompt("n sonini kiriting (n>0)");
+// for (let i = 0; i < n; i++) {
+//   console.log(k);
+// }
+// FOR 2
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting (a<b)");
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+// }
+// console.log(`soni ${b - a + 1}`);
+// FOR 3
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting (a<b)");
+// for (let i = b - 1; i > a; i--) {
+//   console.log(i);
+// }
+// console.log(`soni ${b - a - 1}`);
+// FOR 4
+// let konfet = prompt("1kg konfet narxini kiriting");
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${i}kg ${i * konfet} so'm`);
+// }
+// FOR 5
+// let konfet = prompt("1kg konfet narxini kiriting");
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${i * 100}gr ${(konfet * i) / 10} so'm`);
+// }
