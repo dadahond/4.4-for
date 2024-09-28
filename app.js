d593593aa30d284e21a92d33159cd630fc36b3d7
dadahon <dadahon@gmail.com>
2024-09-28 19:40:05 +0500

    for1-for12

--- app.js
+++ app.js
@@ -28,3 +28,52 @@
 // for (let i = 1; i <= 10; i++) {
 //   console.log(`${i * 100}gr ${(konfet * i) / 10} so'm`);
 // }
+// FOR 6
+// let konfet = prompt("1kg konfet narxini kiriting");
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${i / 10 + 1}kg = ${konfet * (i / 10 + 1)} so'm`);
+// }
+// FOR 7
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting (a<b)");
+// let m = b - a + 1;
+// // m bu a dan b gacha bo'lgan raqamlar soni
+// console.log(((Number(a) + Number(b)) / 2) * m);
+
+// FOR 8
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting (a<b)");
+// let n = 1;
+// for (let i = a; i <= b; i++) {
+//   n *= i;
+// }
+// console.log(`ko'paytmasi ${n} ga teng`);
+// FOR 9
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting (a<b)");
+// let n = 1;
+// for (let i = a; i <= b; i++) {
+//   n += i * i;
+// }
+// console.log(`kvadratlarining yig'indisi ${n - 1} ga teng`);
+// FOR 10
+// let n = prompt("n sonini kiriting (n>0)");
+// let s = 0;
+// for (let i = 1; i <= n; i++) {
+//   s += 1 / i;
+// }
+// console.log(`S = ${s}`);
+// FOR 11
+// let n = prompt("n sonini kiriting (n>0)");
+// let s = 1;
+// for (let i = 1; i <= n; i++) {
+//   s += i * i;
+// }
+// console.log(`S = ${s - 2}`);
+// FOR 12
+// let n = prompt("n sonini kiriting (n>0)");
+// let s = 1;
+// for (let i = 1; i <= n; i++) {
+//   s *= 1 + i / 10;
+// }
+// console.log(`S = ${s}`);
